f3153ebeb83336aed7ec047ee54fa49bf8b668c2
Lara Sanz <larasanz11@gmail.com>
2024-05-29 13:02:28 +0200

    Remove environments from tracking

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  MAPBOX_KEY: "pk.eyJ1IjoibGFyYTExIiwiYSI6ImNsd2M4dXptajB1a20yaXBsNzRyZHFweHUifQ.ijOeBIlcfb4eFdFH9n9kpg"
-
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  producciton: false,
-  MAPBOX_KEY: "pk.eyJ1IjoibGFyYTExIiwiYSI6ImNsd2M4dXptajB1a20yaXBsNzRyZHFweHUifQ.ijOeBIlcfb4eFdFH9n9kpg"
-};
